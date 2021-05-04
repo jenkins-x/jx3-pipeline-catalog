24cf87e986e2f084aef553c4b5cb03605f207f46
Hervé Le Meur <52448671+hervelemeur@users.noreply.github.com>
2021-05-04 13:22:52 +0200

    feat: restart lighthouse pods on pipeline catalog release

--- .lighthouse/jenkins-x/promote.sh
+++ .lighthouse/jenkins-x/promote.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-echo "promoting"
+echo "promoting, restart lighthouse pods"
+kubectl get pods -n jx | grep lighthouse- | awk '{print $1}' | xargs kubectl delete pod -n jx
